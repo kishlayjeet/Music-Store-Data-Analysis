d365cefd372ecfcf67c902500583be57bcfbc48f
Kishlay Jigar <kishlayjeet@MacBook-Air.local>
2023-07-08 21:20:26 +0530

    add data and queries

---
+++ queries/easy_questions.sql
@@ -0,0 +1,38 @@
+/* Question Set 1 - Easy */
+
+-- Q1. Who is the senior most employee based on job title? 
+SELECT * FROM EMPLOYEE WHERE LEVELS = 'L7';
+
+SELECT * FROM EMPLOYEE
+ORDER BY LEVELS DESC
+LIMIT 1;
+
+-- Q2. Which countries have the most Invoices?
+SELECT BILLING_COUNTRY, COUNT(*) AS Most_Invoices FROM INVOICE
+GROUP BY BILLING_COUNTRY
+ORDER BY Most_Invoices DESC;
+
+-- Q3. What are top 3 values of total invoice?
+SELECT TOTAL FROM INVOICE
+ORDER BY TOTAL DESC
+LIMIT 3;
+
+-- Q4. Which city has the best customers? 
+-- A. We would like to throw a promotional Music Festival in the city we made the most money. 
+-- B. Write a query that returns one city that has the highest sum of invoice totals. 
+-- C. Return both the city name & sum of all invoice totals.
+SELECT BILLING_CITY, SUM(TOTAL) AS Invoice_Totals FROM INVOICE
+GROUP BY BILLING_CITY
+ORDER BY Invoice_Totals DESC
+LIMIT 1;
+
+-- Q5. Who is the best customer? 
+-- A. The customer who has spent the most money will be declared the best customer. 
+-- B. Write a query that returns the person who has spent the most money.
+SELECT C.CUSTOMER_ID, C.FIRST_NAME, C.LAST_NAME, SUM(I.TOTAL) AS TOTAL_AMOUNT
+FROM CUSTOMER AS C
+JOIN INVOICE AS I 
+ON C.CUSTOMER_ID = I.CUSTOMER_ID
+GROUP BY C.CUSTOMER_ID
+ORDER BY TOTAL_AMOUNT DESC
+LIMIT 1;
---
+++ queries/moderate_questions.sql
@@ -0,0 +1,37 @@
+/* Question Set 2 - Moderate */
+
+-- Q1. Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
+-- Return your list ordered alphabetically by email starting with A.
+SELECT DISTINCT CUS.EMAIL, CUS.FIRST_NAME, CUS.LAST_NAME
+FROM CUSTOMER AS CUS
+JOIN INVOICE AS INV ON CUS.CUSTOMER_ID = INV.CUSTOMER_ID
+JOIN INVOICE_LINE AS INVL ON INV.INVOICE_ID = INVL.INVOICE_ID
+WHERE INVL.TRACK_ID IN(
+	SELECT T.TRACK_ID
+	FROM TRACK AS T
+	JOIN GENRE AS G ON T.GENRE_ID = G.GENRE_ID
+	WHERE G.NAME LIKE 'Rock'
+)
+ORDER BY CUS.EMAIL;
+
+-- Q2. Let's invite the artists who have written the most rock music in our dataset. 
+-- Write a query that returns the Artist name and total track count of the top 10 rock bands.
+SELECT AR.NAME, COUNT(AR.ARTIST_ID) AS NUMBER_OF_SONGS
+FROM TRACK AS TR 
+JOIN ALBUM AS AL ON TR.ALBUM_ID = AL.ALBUM_ID
+JOIN ARTIST AS AR ON AL.ARTIST_ID = AR.ARTIST_ID
+JOIN GENRE AS GE ON TR.GENRE_ID = GE.GENRE_ID
+WHERE GE.NAME LIKE 'Rock'
+GROUP BY AR.ARTIST_ID
+ORDER BY NUMBER_OF_SONGS DESC LIMIT 10;
+
+-- Q3. Return all the track names that have a song length longer than the average song length. 
+-- A. Return the Name and Milliseconds for each track. 
+-- B. Order by the song length with the longest songs listed first.
+SELECT NAME, MILLISECONDS
+FROM TRACK
+WHERE MILLISECONDS > (
+	SELECT AVG(MILLISECONDS) AS AVG_SONG_LENGTH
+	FROM TRACK
+)
+ORDER BY MILLISECONDS DESC;
